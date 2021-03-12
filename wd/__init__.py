c8c175fdbb173e4cdc7eff5d3824c55ce6dde70c
lyapinadaria <73475798+lyapinadaria@users.noreply.github.com>
2021-03-12 16:15:23 +0300

    Add files via upload

---
+++ wd/__init__.py
@@ -0,0 +1,6 @@
+from __future__ import unicode_literals
+
+from .wd import *
+
+name = 'wd'
+version = '0.0.1'
---
+++ wd/__main__.py
@@ -0,0 +1,10 @@
+from __future__ import print_function, unicode_literals
+
+import wd
+
+def main():
+    print('version is {}'.format(wd.version))
+
+
+if __name__ == '__main__':
+    main()
